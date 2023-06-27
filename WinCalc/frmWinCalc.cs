8ab6c8640db800376903ea7f0d51b456f749fd95
JiradechK <jiradech.ke@sbac.ac.th>
2023-06-27 11:47:27 +0700

    Add project files.

---
+++ WinCalc/frmWinCalc.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinCalc
+{
+    public partial class frmWinCalc : Form
+    {
+        private string number1 = "", number2 = "", answer = "";
+
+        public frmWinCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnZero_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTwo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThree_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFour_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFive_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSix_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSeven_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNine_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
